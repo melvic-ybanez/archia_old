520fe542428d701da7ce311beddc19256380effa
Melvic <melvic.ybanez2014@gmail.com>
2020-03-23 23:54:28 +0800

    Implement match field

--- src/main/scala/com/melvic/archia/ast/fulltext/Params.scala
+++ src/main/scala/com/melvic/archia/ast/fulltext/Params.scala
@@ -2,6 +2,7 @@ package com.melvic.archia.ast.fulltext
 
 import com.melvic.archia.ast.ParamValues.{FuzzinessValue, MinimumShouldMatchValue, RewriteValue}
 import com.melvic.archia.ast.fulltext.Params.MatchField.Operator.OperatorValue
+import com.melvic.archia.ast.fulltext.Params.MatchField.ZeroTermsQuery.ZeroTermsQueryValue
 
 object Params {
   sealed trait MatchField
@@ -17,12 +18,21 @@ object Params {
     final case class Lenient(value: Boolean) extends MatchField
     final case class Operator(value: OperatorValue) extends MatchField
     final case class MinimumShouldMatch(value: MinimumShouldMatchValue) extends MatchField
+    final case class ZeroTermsQuery(value: ZeroTermsQueryValue) extends MatchField
+    final case class CutOffFrequency(value: Float) extends MatchField
 
     object Operator {
       sealed trait OperatorValue
 
       case object Or extends OperatorValue
       case object And extends OperatorValue
+    }
+
+    object ZeroTermsQuery {
+      sealed trait ZeroTermsQueryValue
+
+      case object None extends ZeroTermsQueryValue
+      case object All extends ZeroTermsQueryValue
     }
   }
 }
