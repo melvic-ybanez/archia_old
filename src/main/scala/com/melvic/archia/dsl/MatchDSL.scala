ef9fea4b85db537021fa151f1f8f356bd8550cba
Melvic <melvic.ybanez2014@gmail.com>
2020-03-25 01:53:55 +0800

    Implement value syntax

--- src/main/scala/com/melvic/archia/dsl/MatchDSL.scala
+++
@@ -1,8 +0,0 @@
-package com.melvic.archia.dsl
-
-import com.melvic.archia.ast.fulltext.FullText.Match
-import com.melvic.archia.ast.fulltext.Params.MatchParam.MatchField
-
-trait MatchDSL {
-  def _match(field: MatchField): Match = Match(field)
-}
